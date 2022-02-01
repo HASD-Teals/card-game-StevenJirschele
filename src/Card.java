aaca82929d6e9d700ef79c7d74cbb5d53703267c
StevenJirschele <bmaxx321@gmail.com>
2022-02-01 10:04:16 -0600

    Progress

--- src/Card.java
+++ src/Card.java
@@ -1,18 +1,47 @@
 public class Card {
     // PROPERTIES
     private String color;
-    private char rank;
+    private char symbol;
+    private int rank;
+    private int value;
+    private boolean playable;
 
     // CONSTRUCTORS
-    public Card() {}
+    public Card() {
+        this.color = "";
+        this.symbol = Character.MIN_VALUE;
+        this.rank = 0;
+        this.value = 0;
+        this.playable = false;
+    }
+
+    public Card(String color, char symbol, int rank, int value, boolean playable) {
+        this.color = color;
+        this.symbol = symbol;
+        this.rank = rank;
+        this.value = value;
+        this.playable = playable;
+    }
 
     // ACCESSORS
     public String getColor() {
         return this.color;
     }
 
-    public char getRank() {
+    public char getSymbol() {
+        return this.symbol;
+    }
+
+    public int getRank() {
         return this.rank;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public boolean getPlayable() {
+        return this.playable;
     }
 
     // MUTATORS
@@ -20,9 +49,32 @@ public class Card {
         this.color = color;
     }
 
-    public void setRank(char rank) {
+    public void setSymbol(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public void setRank(int rank) {
         this.rank = rank;
     }
 
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setPlayable(boolean playable) {
+        this.playable = playable;
+    }
+
     // METHODS
+    public String toString() {
+        return this.color + " " + this.rank + " "+ this.symbol + " " + this.value + " " + this.playable + "\n";
+    }
+    public boolean isEqual(Card otherCard) {
+        if(this.value == otherCard.value) {
+            return true;
+        } 
+        else {
+            return false;
+        }
+    }
 }
--- src/CardGameClient.java
+++ src/CardGameClient.java
@@ -1,5 +1,11 @@
 public class CardGameClient {
     public static void main(String[] args) throws Exception {
-        
+    Deck myDeck = new Deck();
+    Card[] Cards = myDeck.getCards();
+    System.out.println(Cards[0].isEqual(Cards[13]));
+    //myDeck.shuffleCards();
+    System.out.println(Cards[0].isEqual(Cards[13]));
+    System.out.println(myDeck.toString());
+    System.out.println(myDeck.cardNum());
     }
 }
