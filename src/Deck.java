6be1e9839696684a330bbcff7e4fe2e9bcb26c77
StevenJirschele <bmaxx321@gmail.com>
2022-02-01 10:04:05 -0600

    Progress

--- src/Deck.java
+++ src/Deck.java
@@ -1,13 +1,44 @@
+import java.util.Random;
 public class Deck {
     // PROPERTIES
     private Card[] cards;
 
     // CONSTRUCTORS
-    public Deck() {}
+    public Deck() {
+        this.cards = new Card[52];
+        int counter = 0;
+        for(int i = 2; i <= 14; i++) {
+            Card temp = new Card("Red", '\u0003', i, i, true);
+            cards[counter] = temp;
+            counter++;
+        }
+        for(int i = 2; i<= 14; i++) {
+            Card temp = new Card("Red", '\u0004', i, i, true);
+            cards[counter] = temp;
+            counter++;
+        }
+        for(int i = 2; i <= 14; i++) {
+            Card temp = new Card("Black", '\u0005', i, i, true);
+            cards[counter] = temp;
+            counter++;
+        }
+        for(int i = 2; i <= 14; i++) {
+            Card temp = new Card("Black", '\u0006', i, i, true);
+            cards[counter] = temp;
+            counter++;
+        }
+    }
 
     // ACCESSORS
     public Card[] getCards() {
         return this.cards;
+    }
+    public int cardNum() {
+        int counter = 0;
+        for(int i = 0; i < this.cards.length; i++) {
+            counter++;
+        }
+        return counter;
     }
 
     // MUTATORS
@@ -18,5 +49,19 @@ public class Deck {
     // METHODS
     public void shuffleCards() {
         // Shuffle this.cards in a random order
+        Random rand = new Random();
+        for(int i = 0; i < this.cards.length; i++) {
+            int random = rand.nextInt(cards.length);
+            Card temp = this.cards[random];
+            this.cards[random] = this.cards[i];
+            this.cards[i] = temp;
+        }
+    }
+    public String toString() {
+        String temp = "";
+        for(Card c:this.cards) {
+            temp += c.toString(); 
+        }
+        return temp;
     }
 }
